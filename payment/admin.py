19cbebef3037a03a03a6f49b46dcb0982212cb41
pochangl <pochanglee2008@gmail.com>
2019-04-18 11:37:03 +0800

    no exception

--- payment/admin.py
+++ payment/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin.utils import flatten_fieldsets
 from .models import Order, PaymentErrorLog
 
 
 class ReadOnlyAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
-        if request.user.is_superuser:
-            return self.readonly_fields
-
         if self.declared_fieldsets:
             return flatten_fieldsets(self.declared_fieldsets)
         else:
@@ -25,4 +23,3 @@ class ErrorAdmin(ReadOnlyAdmin):
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(PaymentErrorLog, ErrorAdmin)
-
