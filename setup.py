bfc7613eb33b638597ffc5e3d24d45b053c2db15
pochangl <pochanglee2008@gmail.com>
2019-04-22 14:37:47 +0800

    version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='Payment',
-    version='0.1.4',
+    version='0.1.5',
     author='Pochang Lee',
     author_email='stupidgod08@yahoo.com.tw',
     description='A django package made for handling third party payment',
